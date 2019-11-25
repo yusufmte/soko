5eec4f1d7b26d87390a8031fdf8874429514f378
Yusuf Ebrahim <yusufmte@gmail.com>
2019-11-25 18:18:56 -0500

    added boulder and hole mechanics!

--- Boulder.gd
+++ Boulder.gd
@@ -1,8 +1,19 @@
 extends Node2D
 
+export var is_topside = true # says whether bould is topside
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+	
+func rotate_bould(dir): # rotates bould (should happen when pushed)
+	if dir == "ccw": $AnimatedSprite.rotate(-PI/2)
+	if dir == "cw": $AnimatedSprite.rotate(PI/2)
+	
+func knock_bould(): # knocks bould down (should happen at holes)
+	$AnimatedSprite.play("falling")
+	is_topside = false
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
--- TileMap.gd
+++ TileMap.gd
@@ -1,18 +1,63 @@
 extends TileMap
 
 var Boulders = preload("res://Boulder.tscn")
+var obstacles = [1,2] # tileid of obstacles (that upon which a boulder cannot be pushed)
+enum {NONE, UP, DOWN, LEFT, RIGHT}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
+	# put boulders in all the peuptiles
 	for cell in get_used_cells_by_id(3):
 		var beuld = Boulders.instance()
 		$Boulders.add_child(beuld)
 		beuld.position = map_to_world(cell) + get_cell_size()/2
 	
-func boulder_here(map_pos):
-	pass
+func get_boulds_here(map_pos): # returns a list containing the boulders at a position
+	var beulds = []
+	for beuld in $Boulders.get_children():
+		if world_to_map(beuld.position) == map_pos: beulds.append(beuld) # adds any boulders there to beulds
+	return beulds # return beulds (it should be empty if no boulder, and have the one boulder if there is one)
 	
+func attempt_bouldrop(bould): # drops a boulder if it's on a hole
+	if get_cellv(world_to_map(bould.position)) == 4:
+		bould.knock_bould()
+
+func push_bould(bould,dir): # moves bould in a certain direction, or returns false if it can't
+	if bould.is_topside:
+		var can_move = false
+		match dir:
+			RIGHT:
+				var dest_coord = world_to_map(bould.position) + Vector2(1,0)
+				var dest_type = get_cellv(dest_coord)
+				if (not dest_type in obstacles) and (get_boulds_here(dest_coord).size() == 0):
+					bould.rotate_bould("cw")
+					bould.position += Vector2(1,0)*cell_size
+					can_move = true
+			LEFT:
+				var dest_coord = world_to_map(bould.position) + Vector2(-1,0)
+				var dest_type = get_cellv(dest_coord)
+				if (not dest_type in obstacles) and (get_boulds_here(dest_coord).size() == 0):
+					bould.rotate_bould("ccw")
+					bould.position += Vector2(-1,0)*cell_size
+					can_move = true
+			UP:
+				var dest_coord = world_to_map(bould.position) + Vector2(0,-1)
+				var dest_type = get_cellv(dest_coord)
+				if (not dest_type in obstacles) and (get_boulds_here(dest_coord).size() == 0):
+					bould.position += Vector2(0,-1)*cell_size
+					can_move = true
+			DOWN:
+				var dest_coord = world_to_map(bould.position) + Vector2(0,1)
+				var dest_type = get_cellv(dest_coord)
+				if (not dest_type in obstacles) and (get_boulds_here(dest_coord).size() == 0):
+					bould.position += Vector2(0,1)*cell_size
+					can_move = true
+		if can_move:
+			attempt_bouldrop(bould)
+		return can_move
+	return false
+					
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 	#pass
