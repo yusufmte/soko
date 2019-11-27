3e9fbeb9a61faa92333772df440c3d262bc02264
Yusuf Ebrahim <yusufmte@gmail.com>
2019-11-26 22:06:06 -0500

    added menu and level selector and restructured file tree

--- Main.gd
+++ Main.gd
@@ -1,48 +1,119 @@
 extends Node
-var Level = preload("res://TileMap.tscn")
+
+var current_level = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	_on_HUD_restart()
-	$HUD/Panel/RestartButt.connect("pressed",self,"_on_HUD_restart")
-	$HUD/Panel/QuitButt.connect("pressed",self,"_on_HUD_quit")
+	
+	# connect buttons
+	$Menu/QuitButt.connect("pressed",self,"quit_button")
+	$HUD/RestartButt.connect("pressed",self,"reload_level")
+	$HUD/MenuButt.connect("pressed",self,"return_to_menu")
+	$HUD/QuitButt.connect("pressed",self,"quit_button")
+	
+	# populate level select box
+	for i in range(num_levels()-1):
+		$Menu/LevelSelectBox.add_item("Level "+str(i+1))
+		
+	$Menu/LevelSelectBox.connect("item_activated",self,"_on_level_selected")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
-func _on_HUD_restart(): # when restart button is clicked
-	for child in get_children(): # if there's a level, remove it
-		if child.get_name() == "level":
-			child.free() # this should be queue_free(), but that results in player duplication...pls remember to address this issue later
+func num_levels(): # returns number of levels
+	var level_count = 1
+	var file = File.new()
+	while file.file_exists("res://levels/level"+str(level_count)+".tscn"):
+		level_count += 1
+	return level_count
+
+func _on_level_selected(lvl): # when a level is activated through the selector
+	load_level(lvl+1) # need to add one because of the indices in the selector box
+	
+func load_level(lvl): # when load a new level
+
+	hide_menu()
+	
+	current_level = lvl
+	
+	# deload current level, then load new one
+	deload_level()
+	var Level = load("res://levels/level"+str(lvl)+".tscn")
 	var level = Level.instance()
-	level.set_name("level")
+	level.set_name("current_level")
 	add_child(level) # then make a new one
+	
+	# reset HUD
+	$HUD/MsgLabel.text = ""
+	$HUD/MoveCounter.text = "0"
+	$HUD/LvlLabel.text = "Level "+str(lvl)
+	show_HUD()
 	
 	# connect all the signals
 	level.connect("player_moved",self,"_on_player_moved")
 	level.connect("victory",self,"_on_victory")
 	level.connect("defeat",self,"_on_defeat")
 	level.connect("pyrrhic_victory",self,"_on_pyrrhic_victory")
+	level.connect("bouldpush",self,"_on_boulder_pushed")
+	level.connect("knock",self,"_on_thing_knocked")
 	
-	# reset label text
-	$HUD/Panel/MsgLabel.text = ""
-	$HUD/Panel/MoveLabel/MoveCounter.text = "0"
-	
-func _on_HUD_quit(): # quit button pressed
+func deload_level():
+	for child in get_children(): # if there's a level, remove it
+		if child.get_name() == "current_level":
+			child.free() # this should be queue_free(), but that results in player duplication...pls remember to address this issue later
+	for audio in $AudioManager.get_children():
+		audio.stop() # stop any currently playing audio
+	hide_HUD()
+
+func reload_level():
+	load_level(current_level)
+
+func return_to_menu():
+	deload_level()
+	show_menu()
+
+func quit_button(): # quit button pressed
 	get_tree().quit() # quit the current scene tree (should exit game)
+
+func show_HUD():
+	$HUD.show()
+	for label in $HUD.get_children():
+		label.show()
+
+func show_menu():
+	$Menu.show()
+	for label in $Menu.get_children():
+		label.show()
+	
+func hide_HUD():
+	for label in $HUD.get_children():
+		label.hide()
+	$HUD.hide()
+
+func hide_menu():
+	for label in $Menu.get_children():
+		label.hide()
+	$Menu.hide()
 	
 func _on_player_moved(): # when the player changes position
-	$HUD/Panel/MoveLabel/MoveCounter.text = str(int($HUD/Panel/MoveLabel/MoveCounter.text)+1) # increment movecounter
+	$HUD/MoveCounter.text = str(int($HUD/MoveCounter.text)+1) # increment movecounter
+	$AudioManager/walk_sound.play()
+
+func _on_boulder_pushed(): # when the player pushes a boulder
+	$AudioManager/push_sound.play()
+	
+func _on_thing_knocked(): # when a beuld is knocked
+	$AudioManager/knock_sound.play()
 	
 func _on_victory(): # when win
-	$HUD/Panel/MsgLabel.text = "You won!"
+	$HUD/MsgLabel.text = "You won!"
 	$AudioManager/victory_sound.play()
 	
 func _on_defeat(): # when lose
-	$HUD/Panel/MsgLabel.text = "You lost..."
+	$HUD/MsgLabel.text = "You lost..."
 	$AudioManager/defeat_sound.play()
 
 func _on_pyrrhic_victory(): # when win, then get peuped
-	$HUD/Panel/MsgLabel.text = "You...won?"
+	$HUD/MsgLabel.text = "You...won?"
 	$AudioManager/pyrrhic_victory_sound.play()
