abc27a962868d9fec116cb35fcc6e27005ff6f54
Yusuf Ebrahim <yusufmte@gmail.com>
2019-11-26 23:04:49 -0500

    added triangular beulds, spawnpoints, and holes

--- levels/lvl_template/Boulder.gd
+++ levels/lvl_template/Boulder.gd
@@ -1,16 +1,37 @@
 extends Node2D
 
 export var is_topside = true # says whether bould is topside
+export var bould_type = "c"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+
+func change_bould_type(new_type):
+	bould_type = new_type
+	if bould_type == "c":
+		$CBoulderSprite.show()
+		$TBoulderSprite.hide()
+	elif bould_type == "t":
+		$TBoulderSprite.show()
+		$CBoulderSprite.hide()
+
+func switch_bould_type():
+	if bould_type == "c":
+		change_bould_type("t")
+	elif bould_type == "t":
+		change_bould_type("c")
 	
 func rotate_bould(dir): # rotates bould (should happen when pushed)
-	if dir == "ccw": $BoulderSprite.rotate(-PI/2)
-	if dir == "cw": $BoulderSprite.rotate(PI/2)
+	if bould_type == "c":
+		$CBoulderSprite.rotate(dir*PI/2)
+	elif bould_type == "t":
+		$TBoulderSprite.rotate(2*PI/3)
 	
 func knock_bould(): # knocks bould down (should happen at holes)
-	$FallAnimation.play("fall")
+	if bould_type == "c":
+		$FallAnimation.play("cfall")
+	elif bould_type == "t":
+		$FallAnimation.play("tfall")
 	is_topside = false
 
