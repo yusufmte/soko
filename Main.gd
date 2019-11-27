86ac219742b9ddc921edc0b73aa52516f24c3dfb
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2019-11-26 23:53:03 -0800

    add LevelLoader node to handle creation and deletion of levels
    The purpose of this Node is to have something that will have levels as children
    (one level as its only child, hopefully)
    It makes sense to also have it handle creation and deletion of levels, rather than do
    that directly in Main

--- Main.gd
+++ Main.gd
@@ -13,45 +13,29 @@ func _ready():
 	$HUD.connect("quit",self,"quit_button")
 
 
-
 func load_level(lvl): # when load a new level
-
 	$Menu.hide()
 	
 	current_level = lvl
 	
-	# deload current level, then load new one
-	deload_level()
-	var Level = load("res://levels/level"+str(lvl)+".tscn")
-	var level = Level.instance()
-	level.set_name("current_level")
-	add_child(level) # then make a new one
+	$LevelLoader.deload_level()
+	for audio in $AudioManager.get_children():
+		audio.stop() # stop any currently playing audio
+	
+	$LevelLoader.load_level(lvl)
 	
 	# reset HUD
 	$HUD.reset(lvl)
 	$HUD.show()
-	
-	# connect all the signals
-	level.connect("player_moved",self,"_on_player_moved")
-	level.connect("victory",self,"_on_victory")
-	level.connect("defeat",self,"_on_defeat")
-	level.connect("pyrrhic_victory",self,"_on_pyrrhic_victory")
-	level.connect("bouldpush",self,"_on_boulder_pushed")
-	level.connect("knock",self,"_on_thing_knocked")
-	
-func deload_level():
-	for child in get_children(): # if there's a level, remove it
-		if child.get_name() == "current_level":
-			child.free() # this should be queue_free(), but that results in player duplication...pls remember to address this issue later
-	for audio in $AudioManager.get_children():
-		audio.stop() # stop any currently playing audio
-	$HUD.hide()
 
 func reload_level():
 	load_level(current_level)
 
 func return_to_menu():
-	deload_level()
+	$LevelLoader.deload_level()
+	for audio in $AudioManager.get_children():
+		audio.stop() # stop any currently playing audio
+	$HUD.hide()
 	$Menu.show()
 
 func quit_button(): # quit button pressed
